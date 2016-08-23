4586999560e82bd76a4ed89044e032ad28d7a4e7
MigielDV <migiel@surfnet.nl>
2016-08-23 16:46:31 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Merge NetherLight testing files"
-cat /root/regulartesting.conf /etc/perfsonar/regulartesting.conf > /etc/perfsonar/regulartesting.conf
+echo -e "$(cat /root/regulartesting.conf)\n$(cat /etc/perfsonar/regulartesting.conf)" > /etc/perfsonar/regulartesting.conf
 echo "Starting OWAMP..."
 /etc/init.d/owamp-server start
 echo "Starting BWCTL..."
